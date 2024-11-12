5078a0c4eb9f11a2cb840586b5184322a22cc9ed
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2024-11-12 19:06:14 +0330

    forward method added

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 class InputEmbedding(nn.Module):
     def __init__(self, d_model:int, vocab_size:int):
@@ -8,4 +9,8 @@ class InputEmbedding(nn.Module):
         self.d_model = d_model
         self.embedding = nn.Embedding(vocab_size, d_model)
     
+    def forward(self,  x):
+        return self.embedding(x) * math.sqrt(self.d_model)
     
+
+
